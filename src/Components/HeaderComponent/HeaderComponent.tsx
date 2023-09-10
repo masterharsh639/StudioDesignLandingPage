3323a2d19c1c10c9f17199082ba9811ead3d9e4d
harshmishraskugal <harshmishra@skugal.com>
2023-09-10 17:31:14 +0530

    first commit

--- src/Components/HeaderComponent/HeaderComponent.tsx
+++ src/Components/HeaderComponent/HeaderComponent.tsx
@@ -1,15 +1,19 @@
-import { Grid } from "@mui/material";
+import { Grid, Link } from "@mui/material";
+import './headercomponent.css';
 
 const HeaderComponent = () => {
   return (
-    <Grid container xs={6}>
-      <Grid item xs={4}>
-        <p>
-          <a href="">A+ Studio</a>
-        </p>
+    <Grid container xs={12} className="page-section header-section">
+      <Grid item xs={5} className="header-logo">
+        <Link href="#">A+ Studio</Link>
       </Grid>
-      <Grid item xs={5}>
-
+      <Grid item xs={5} className="header-navs">
+        <Link className="p-18">Home</Link>
+        <Link>What We Do</Link>
+        <Link>Service</Link>
+        <Link>Project</Link>
+        <Link>Blog</Link>
+        <Link>Contact</Link>
       </Grid>
     </Grid>
   );
